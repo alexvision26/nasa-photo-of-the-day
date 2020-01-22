30b2a1bc20802f8ba90991a7bc6a138189057fa1
Alex Miller <alexmillerprojects@gmail.com>
2020-01-22 14:31:30 -0800

    finished project, working on stretch

--- src/components/InfoContainer.js
+++ src/components/InfoContainer.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const InfoContainer = props => {
-    console.log(props)
+    // console.log(props)
     return (
         <div className='sidebar'>
             <h2>{props.data.title} </h2>
-            <p>Description: {props.data.explanation}</p>
+            <p><strong>Description:</strong> {props.data.explanation}</p>
             <h4>Today's Date: {props.data.date}</h4>
             <h4>Credit: {props.data.copyright}</h4>
         </div>
